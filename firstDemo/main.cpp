9db74db2f7087e60563bb16c2d66b9ec8560e6ee
ouyinlu <43701230+ouyinlu@users.noreply.github.com>
2018-12-25 08:20:16 +0800

    Add files via upload

---
+++ firstDemo/main.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+using namespace std;
+#include"./gdal/gdal_priv.h"
+#pragma comment(lib,"gdal_i.lib")
+
+int main() {
+	//输入图像
+	GDALDataset* poSrcDS;
+	//输出图像
+	GDALDataset* poDstDS;
+	//图像的宽度和高度
+	int imgXlen, imgYlen;
+	//输出图像路径
+	char* srcPath = "May Wind.jpeg";
+	//图像内部存储
+	char* dstPath = "res.tif";
+	//图像内部存储
+	GByte* buffTmp;
+	//图像波段数
+	int i, bandNum;
+
+	//注册驱动
+	GDALAllRegister();
+
+	//打开图像
+	poSrcDS = (GDALDataset*)GDALOpenShared(srcPath, GA_ReadOnly);
+
+	//获取图像的宽度、高度和波段数量
+	imgXlen = poSrcDS->GetRasterXSize();
+	imgYlen = poSrcDS->GetRasterYSize();
+	bandNum = poSrcDS->GetRasterCount();
+	//获取输出的结果
+	cout << "Img X Length: " << imgXlen << endl;
+	cout << "Img Y Length: " << imgYlen << endl;
+	cout << "Band number: " << bandNum << endl;
+	//根据图像的宽度和高度分配内存
+	buffTmp = (GByte*)CPLMalloc(imgXlen*imgYlen * sizeof(GByte));
+	//创建输出图像
+	poDstDS = GetGDALDriverManager()->GetDriverByName("GTiff")->Create(
+		dstPath, imgXlen, imgYlen, bandNum, GDT_Byte, NULL);
+	//一个个波段的输入，然后一个个波段的输出
+	for (i = 0; i < bandNum; i++) {
+		poSrcDS->GetRasterBand(i + 1)->RasterIO(GF_Read,
+			0, 0, imgXlen, imgYlen, buffTmp, imgXlen, imgYlen, GDT_Byte, 0, 0);
+		poDstDS->GetRasterBand(i + 1)->RasterIO(GF_Write,
+			0, 0, imgXlen, imgYlen, buffTmp, imgXlen, imgYlen, GDT_Byte, 0, 0);
+		cout << "... ... band " << i << " processing ... ..." << endl;
+	}
+
+	//清除内存
+	CPLFree(buffTmp);
+	//关闭dataset
+	GDALClose(poDstDS);
+	GDALClose(poSrcDS);
+
+	system("PAUSE");
+	return 0;
+}
